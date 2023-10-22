26327eda30b9262696775bfdc9227b1f049cf25f
aazkgh <gg25hp17@naver.com>
2023-10-23 04:01:19 +0900

    bank 필터링 추가

--- jsbank/initial.js
+++ jsbank/initial.js
@@ -4,24 +4,24 @@ export const HISTORY_LIST = [
         category: "Food",
         title: "GS25 코엑스점",
         type: "output",
-        amount: "3100", 
+        amount: 3100, 
     },
     {   
         category: "Hobby",
         title: "양재 화훼시장",
         type: "output",
-        amount: "12000", 
+        amount: 12000, 
     },
     {   
         category: "Work",
         title: "현대 백화점",
         type: "input",
-        amount: "120000", 
+        amount: 120000, 
     },
     {   
         category: "Shop",
         title: "지그재그",
         type: "output",
-        amount: "24500", 
+        amount: 24500, 
     }
 ];
--- jsbank/main.js
+++ jsbank/main.js
@@ -1,10 +1,13 @@
 import {INIT_BALANCE, HISTORY_LIST} from './initial.js';
 
+/*초기 렌더링*/
 const listTotal = document.querySelector("ul");
 
 HISTORY_LIST.forEach((element)=>{
     const list = document.createElement("li");
-    list.className = "list";
+    element.type === "input"
+    ? list.className = "list_input"
+    : list.className = "list_output";
 
     const amount = element.type === "input" ? `+${element.amount}` : `-${element.amount}`;
 
@@ -20,3 +23,57 @@ HISTORY_LIST.forEach((element)=>{
     `;
     listTotal.appendChild(list);
 });
+
+let totalInput = INIT_BALANCE;
+let totalOutput = INIT_BALANCE;
+let totalAsset = INIT_BALANCE;
+
+HISTORY_LIST.forEach((element)=>{
+    element.type === "input"
+    ? totalInput += element.amount
+    : totalOutput += element.amount
+});
+
+totalAsset = totalInput-totalOutput;
+
+document.querySelector(".total_asset").innerHTML = `${totalAsset}`;
+document.querySelector(".total_inout :nth-child(2)").innerHTML = `${totalInput}`; 
+document.querySelector(".total_inout :nth-child(4)").innerHTML = `${totalOutput}`;
+
+/*버튼 필터링*/
+const inputBox = document.getElementById("input_box");
+const outputBox = document.getElementById("output_box");
+
+function filtering(){
+    const inputList = document.querySelectorAll(".list_input");
+    const outputList = document.querySelectorAll(".list_output");
+
+    if(inputBox.checked){
+        showList(inputList);
+    }
+    else{
+        hideList(inputList);
+    }
+
+    if(outputBox.checked){
+        showList(outputList);
+    }    
+    else{
+        hideList(outputList);
+    }  
+}
+
+function showList(list) {
+    list.forEach((element) => {
+        element.style.display = "flex";
+    });
+}
+  
+function hideList(list) {
+    list.forEach((element) => {
+        element.style.display = "none";
+    });
+}
+
+inputBox.addEventListener('change', filtering);
+outputBox.addEventListener('change', filtering);
