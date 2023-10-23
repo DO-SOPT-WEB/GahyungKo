b6c0f2e0b9523567ff7d6d14a8c5b96657563c79
aazkgh <gg25hp17@naver.com>
2023-10-23 17:07:51 +0900

    리스트 삭제,추가 함수 구현 및 x 버튼 활성화

--- jsbank/main.js
+++ jsbank/main.js
@@ -3,42 +3,17 @@ import {INIT_BALANCE, HISTORY_LIST} from './initial.js';
 /*초기 렌더링*/
 const listTotal = document.querySelector("ul");
 
-HISTORY_LIST.forEach((element)=>{
-    const list = document.createElement("li");
-    element.type === "input"
-    ? list.className = "list_input"
-    : list.className = "list_output";
+let tempInput = INIT_BALANCE;
+let tempOutput = INIT_BALANCE;
+let tempAsset = INIT_BALANCE;
 
-    const amount = element.type === "input" ? `+${element.amount}` : `-${element.amount}`;
-
-    list.innerHTML = `
-        <span>${element.category}</span>
-        <span>${element.title}</span>
-        <span class = "${
-            element.type === "input" ?
-            "input" :
-            "output"
-        }">${amount}</span>
-        <button type="button" class="list_btn">x</button>    
-    `;
-    listTotal.appendChild(list);
-});
-
-let totalInput = INIT_BALANCE;
-let totalOutput = INIT_BALANCE;
-let totalAsset = INIT_BALANCE;
+const totalInput = document.querySelector(".total_inout :nth-child(2)");
+const totalOutput = document.querySelector(".total_inout :nth-child(4)");
+const totalAsset = document.querySelector(".total_asset");
 
 HISTORY_LIST.forEach((element)=>{
-    element.type === "input"
-    ? totalInput += element.amount
-    : totalOutput += element.amount
+    addList(element);
 });
-
-totalAsset = totalInput-totalOutput;
-
-document.querySelector(".total_asset").innerHTML = `${totalAsset}`;
-document.querySelector(".total_inout :nth-child(2)").innerHTML = `${totalInput}`; 
-document.querySelector(".total_inout :nth-child(4)").innerHTML = `${totalOutput}`;
 
 /*버튼 필터링*/
 const inputBox = document.getElementById("input_box");
@@ -63,6 +38,7 @@ function filtering(){
     }  
 }
 
+/*필터링시 display 변경 함수*/
 function showList(list) {
     list.forEach((element) => {
         element.style.display = "flex";
@@ -77,3 +53,63 @@ function hideList(list) {
 
 inputBox.addEventListener('change', filtering);
 outputBox.addEventListener('change', filtering);
+
+/*리스트 추가 및 삭제 함수*/
+function addList(element){
+    const list = document.createElement("li");
+    element.type === "input"
+    ? list.className = "list_input"
+    : list.className = "list_output";
+
+    const amount = element.type === "input" ? `+${element.amount}` : `-${element.amount}`;
+
+    list.innerHTML = `
+        <span>${element.category}</span>
+        <span>${element.title}</span>
+        <span class = "${
+            element.type === "input" ?
+            "input" :
+            "output"
+        }">${amount}</span>
+        <button type="button" class="list_btn">x</button>    
+    `;
+    list.children[3].addEventListener('click', deleteList);
+    
+    //자산 계산
+    element.type === "input"
+    ? tempInput += element.amount
+    : tempOutput += element.amount
+
+    tempAsset = tempInput-tempOutput;
+
+    totalInput.innerHTML = `${tempInput}`; 
+    totalOutput.innerHTML = `${tempOutput}`;
+    totalAsset.innerHTML = `${tempAsset}`;
+
+    listTotal.appendChild(list);
+}
+
+function deleteList(event){
+    const motherList = event.target.parentElement;
+    const list = motherList.children[2];
+    const element = {};
+
+    list.className === "input"
+    ? element.type = "input"
+    : element.type = "output";
+
+    element.amount = Math.abs(list.innerHTML);
+
+    //자산 계산
+    element.type === "input"
+    ? tempInput -= element.amount
+    : tempOutput -= element.amount
+
+    tempAsset = tempInput-tempOutput;
+
+    totalInput.innerHTML = `${tempInput}`; 
+    totalOutput.innerHTML = `${tempOutput}`;
+    totalAsset.innerHTML = `${tempAsset}`;
+
+    motherList.remove();
+}
