78b8f37a031671257589591eadc59ca2558b0d03
aazkgh <gg25hp17@naver.com>
2023-10-24 01:54:38 +0900

    기본 내역 추가 모달 구현 완료(기본 과제 완료)

--- jsbank/initial.js
+++ jsbank/initial.js
@@ -1,25 +1,25 @@
 export const INIT_BALANCE = 0;
 export const HISTORY_LIST = [
     {   
-        category: "Food",
+        category: "음식",
         title: "GS25 코엑스점",
         type: "output",
         amount: 3100, 
     },
     {   
-        category: "Hobby",
-        title: "양재 화훼시장",
+        category: "음식",
+        title: "그릭 요거트",
         type: "output",
         amount: 12000, 
     },
     {   
-        category: "Work",
+        category: "알바",
         title: "현대 백화점",
         type: "input",
         amount: 120000, 
     },
     {   
-        category: "Shop",
+        category: "쇼핑",
         title: "지그재그",
         type: "output",
         amount: 24500, 
--- jsbank/main.js
+++ jsbank/main.js
@@ -113,3 +113,75 @@ function deleteList(event){
 
     motherList.remove();
 }
+
+/*모달 여닫는 버튼 추가*/
+const modal_wrapper =document.querySelector(".modal_wrapper");
+const modalBtn = document.getElementById("add_btn");
+const closeBtn = document.getElementById("close")
+
+modalBtn.onclick = () => {
+    modal_wrapper.style.display = "flex";
+};
+closeBtn.onclick = () => {
+    modal_wrapper.style.display = "none";
+};
+
+/*모달 체크 박스 설정 및 카테고리 연동*/
+const modalInbox = document.getElementById("input_modalbox");
+const modalOutbox = document.getElementById("output_modalbox");
+const modalInput = document.getElementById("select_input");
+const modalOutput = document.getElementById("select_output");
+
+let newCategory = document.getElementById("category_input");
+let tempType = null;
+
+function checkOnly(e){
+    if(e.target === modalInbox){
+        modalInbox.checked = true;
+        modalOutbox.checked = false;
+
+        modalInput.style.display = "flex";
+        modalOutput.style.display = "none";
+
+        tempType = "input";
+        newCategory = document.getElementById("category_input");
+    }
+    else{
+        modalInbox.checked = false;
+        modalOutbox.checked = true;
+
+        modalInput.style.display = "none";
+        modalOutput.style.display = "flex";
+
+        tempType = "output";
+        newCategory = document.getElementById("category_output");
+    }
+}
+
+modalInbox.addEventListener('click', checkOnly);
+modalOutbox.addEventListener('click', checkOnly);
+
+/*리스트 새로 저장하기*/
+const saveBtn = document.getElementById("save");
+
+const newAmount = document.getElementById("amount");
+const newTitle = document.getElementById("title");
+
+function newList(){
+    let tempCategory = newCategory.value;
+    let tempAmount = newAmount.value;
+    let tempTitle = newTitle.value;
+
+    let newObject = {
+        category: tempCategory,
+        title: tempTitle,
+        type: tempType,
+        amount: parseInt(tempAmount), 
+    }
+
+    addList(newObject);
+
+    alert("새로운 내역 저장 완료!");
+}
+
+saveBtn.addEventListener('click', newList);
