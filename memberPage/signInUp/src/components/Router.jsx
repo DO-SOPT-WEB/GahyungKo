a32edeb66d11e862abe0bd7ab5550e0b2b6eb068
aazkgh <gg25hp17@naver.com>
2023-11-16 22:45:23 +0900

    리다이렉트 기능 추가

--- memberPage/signInUp/src/components/Router.jsx
+++ memberPage/signInUp/src/components/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import SignUp from "../page/signup";
 import LogIn from "../page/login";
 import MyPage from "../page/mypage";
@@ -7,9 +7,10 @@ const Router = () => {
     return (
       <BrowserRouter>
         <Routes>
-            <Route path="/login" element={<LogIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/mypage/:userId" element={<MyPage />} />
+          <Route path="/" element={<Navigate replace to="/login" />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/mypage/:userId" element={<MyPage />} />
         </Routes>
       </BrowserRouter>
     );
