e17d0520b380c25beedc7a7fd63ccfe59981931d
aazkgh <gg25hp17@naver.com>
2023-12-11 18:09:05 +0900

    refactor: 코드리뷰 반영

--- memberPage/signInUp/src/page/signup.jsx
+++ memberPage/signInUp/src/page/signup.jsx
@@ -1,93 +1,139 @@
-import { useState, useEffect } from 'react';
-import { Modal, Header, LoginInput, InputTitle, Input, Button, SignUpBtn, InputContainer } from "../styles";
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import {
+  Modal,
+  Header,
+  LoginInput,
+  InputTitle,
+  Input,
+  Button,
+  SignUpBtn,
+  InputContainer,
+} from "../styles";
+import { useNavigate } from "react-router-dom";
 
-import axios from 'axios';
+import axios from "axios";
 
 const SignUp = () => {
-    const [btnState, setBtnState] = useState(false);
-    const [exist, setExist] = useState(null);
+  const [activeSignUp, setActiveSignUp] = useState(false);
+  const [exist, setExist] = useState(null);
 
-    const [id, setId] = useState('');
-    const [pw, setPw] = useState('');
-    const [checkPw, setCheckPw] = useState('');
-    const [nickname, setNickname] = useState('');
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const [checkPw, setCheckPw] = useState("");
+  const [nickname, setNickname] = useState("");
 
-    const navigate = useNavigate();
-    
-    //회원 가입 버튼 활성화
-    useEffect(() => {
-        if ( id && pw && checkPw && nickname 
-            && checkPw === pw && exist === false) {
-            setBtnState(true);
+  const navigate = useNavigate();
+
+  //회원 가입 버튼 활성화
+  useEffect(() => {
+    id && pw && checkPw && nickname && checkPw === pw && exist === false
+      ? setActiveSignUp(true)
+      : setActiveSignUp(false);
+  }, [id, pw, checkPw, nickname, exist]);
+
+  //아이디 이벤트 발생시 작동하는 함수
+  const idEvent = (event) => {
+    setId(event.target.value);
+    setExist(null);
+    setActiveSignUp(false);
+  };
+
+  //아이디 중복확인
+  const idCheck = async () => {
+    try {
+      const exist = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/api/v1/members/check?username=${id}`
+      );
+      setExist(exist.data.isExist);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //가입할 회원 데이터 전달
+  const signUpHandler = async () => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BASE_URL}/api/v1/members`,
+        {
+          username: id,
+          password: pw,
+          nickname: nickname,
         }
-        else
-            setBtnState(false);
-      }, [id, pw, checkPw, nickname, exist]
-    );
+      );
+      response && navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    //아이디 이벤트 발생시 작동하는 함수
-    const idEvent = (event) => {
-        setId(event.target.value); 
-        setExist(null); 
-        setBtnState(false);
-    };
-
-    //아이디 중복확인
-    const idCheck = async () => {
-        try {
-            const exist = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/v1/members/check?username=${id}`);
-            setExist(exist.data.isExist);
-        } catch (err) {
-            console.log(err);
-        }
-    };
-    
-    //가입할 회원 데이터 전달
-    const signUphandler = async () => {
-        try {
-            const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/api/v1/members`, {
-                username: id,
-                password: pw,
-                nickname: nickname, 
-            });
-            response && navigate('/login');
-      } catch (err) {
-            console.log(err);
-      }
-    };
-
-    return (
-        <Modal>
-            <Header>Sign Up</Header>
-            <InputContainer>
-                <LoginInput>
-                    <InputTitle>ID</InputTitle>
-                    <Input type="text" name='id' id='id'
-                    onChange={(e) => {idEvent(e)}}/>
-                    <Button className={exist === true ? "exist" : exist === false ? "notExist" : null }
-                    type="button" onClick={idCheck}>중복체크</Button>
-                </LoginInput>
-                <LoginInput>
-                    <InputTitle>비밀번호</InputTitle>
-                    <Input type="text" name='password' id='password'
-                    onChange={(e) => setPw(e.target.value)}/>
-                </LoginInput>
-                <LoginInput>
-                    <InputTitle>비밀번호 확인</InputTitle>
-                    <Input type="text" name='pwCheck' id='pwCheck'
-                    onChange={(e) => setCheckPw(e.target.value)}/>
-                </LoginInput>
-                <LoginInput>
-                    <InputTitle>닉네임</InputTitle>
-                    <Input type="text" name='name' id='name'
-                    onChange={(e) => setNickname(e.target.value)}/>
-                </LoginInput>
-            </InputContainer>  
-            <SignUpBtn className={ btnState ? null : "inactive"}
-            disabled={!btnState} onClick={signUphandler}>회원 가입</SignUpBtn>
-        </Modal>
-    );
-}
+  return (
+    <Modal>
+      <Header>Sign Up</Header>
+      <InputContainer>
+        <LoginInput>
+          <InputTitle>ID</InputTitle>
+          <Input
+            type="text"
+            name="id"
+            id="id"
+            onChange={(e) => {
+              idEvent(e);
+            }}
+          />
+          <Button
+            className={
+              exist === true ? "exist" : exist === false ? "notExist" : null
+            }
+            type="button"
+            onClick={idCheck}
+          >
+            중복체크
+          </Button>
+        </LoginInput>
+        <LoginInput>
+          <InputTitle>비밀번호</InputTitle>
+          <Input
+            type="text"
+            name="password"
+            id="password"
+            onChange={(e) => {
+              setPw(e.target.value);
+            }}
+          />
+        </LoginInput>
+        <LoginInput>
+          <InputTitle>비밀번호 확인</InputTitle>
+          <Input
+            type="text"
+            name="pwCheck"
+            id="pwCheck"
+            onChange={(e) => {
+              setCheckPw(e.target.value);
+            }}
+          />
+        </LoginInput>
+        <LoginInput>
+          <InputTitle>닉네임</InputTitle>
+          <Input
+            type="text"
+            name="name"
+            id="name"
+            onChange={(e) => {
+              setNickname(e.target.value);
+            }}
+          />
+        </LoginInput>
+      </InputContainer>
+      <SignUpBtn
+        className={!activeSignUp && "inactive"}
+        disabled={!activeSignUp}
+        onClick={signUpHandler}
+      >
+        회원 가입
+      </SignUpBtn>
+    </Modal>
+  );
+};
 
 export default SignUp;
