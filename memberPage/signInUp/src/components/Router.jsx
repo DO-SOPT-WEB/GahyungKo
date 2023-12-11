6b09f63b516216a7df2a69d036eb1a09136aed4e
aazkgh <gg25hp17@naver.com>
2023-12-11 17:46:52 +0900

    refactor: 파일명 변경

--- memberPage/signInUp/src/components/Router.jsx
+++ memberPage/signInUp/src/components/Router.jsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import SignUp from "../page/signup";
-import LogIn from "../page/login";
-import MyPage from "../page/mypage";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import SignUp from "../page/SignUp";
+import LogIn from "../page/LogIn";
+import MyPage from "../page/MyPage";
 
 const Router = () => {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/login" />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/mypage/:userId" element={<MyPage />} />
-        </Routes>
-      </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/mypage/:userId" element={<MyPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
